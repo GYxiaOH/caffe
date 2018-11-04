3246196166557cb0570dd3987e0e11d0013a3007
GYxiaOH <zhhy1994226@163.com>
2018-11-04 21:13:10 +0800

    fix create
    change default GPU to 0

--- models/TIP/create_class.sh
+++ models/TIP/create_class.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-LOG=/home/hongyuan/l2/-`date +%Y-%m-%d-%H-%M-%S`.log
+#LOG=/home/hongyuan/l2/-`date +%Y-%m-%d-%H-%M-%S`.log #the path of log file
 
-/home/hongyuan/zhy/caffe/build/tools/caffe train --weights=/home/hongyuan/zhy/caffe/models/vggs/VGG_CNN_S_2.caffemodel --solver=./solver_click_median.prototxt --gpu 2 2>&1 | tee $LOG 
+/home/hongyuan/zhy/caffe/build/tools/caffe train --weights=/home/hongyuan/zhy/caffe/models/vggs/VGG_CNN_S_2.caffemodel --solver=./solver_click_median.prototxt --gpu 0 2>&1 | tee $LOG 
